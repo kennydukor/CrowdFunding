73eacfcbd91455506ee7a8ff523b6d882c7bbd7a
Kenechi <kennydukor@gmail.com>
2024-12-30 12:02:29 -0500

    fixing the deployment issues

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use('/api/payments', paymentRoutes);
 // mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB Connected...'))
-    .catch(err => console.error(err));
+    .catch(err => console.error('MongoDB Connection Error;', err.message));
 
 // COMMENT THIS OUT IF NOT USING VERCEL
 module.exports = app;
