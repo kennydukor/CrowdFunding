14d35ec96016bf400027001063b13e8d7f7bbce3
Kenechi <kennydukor@gmail.com>
2024-12-30 11:50:01 -0500

    fixing the deployment issues

--- vercel-server.js
+++ vercel-server.js
@@ -1,3 +1,15 @@
+// const serverless = require('serverless-http');
+// const app = require('./server'); // Import your existing Express app
+// module.exports = serverless(app);
+
 const serverless = require('serverless-http');
-const app = require('./server'); // Import your existing Express app
-module.exports = serverless(app);
+const app = require('./server'); // Import your Express app
+
+const handler = serverless(app, {
+  request: (req) => {
+    delete req.headers['content-length']; // Workaround for content-length issues
+    return req;
+  },
+});
+
+module.exports = handler;
