2e981d4c7983bd05185b8ff974a5e04509cf7e68
Kenechi <kennydukor@gmail.com>
2024-12-30 11:27:25 -0500

    fixing deployment issue

---
+++ api/index.js
@@ -0,0 +1,5 @@
+const app = require('./server');
+
+const serverless = require('serverless-http');
+
+module.exports = serverless(app);
---
+++ api/readme.txt
@@ -0,0 +1,4 @@
+NOTE:
+
+The app folder was only created for vercel. 
+The original approach was to use the server.js in the root
