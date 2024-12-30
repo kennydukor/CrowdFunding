3597d9fd9c1a4df5049157291f10afb2f8e13451
Kenechi <kennydukor@gmail.com>
2024-12-30 12:48:28 -0500

    still trying to fix vercel

--- server.js
+++ server.js
@@ -18,8 +18,15 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(express.json());
+// app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(morgan('dev'));
+
+// Health Check Route
+app.get('/', (req, res) => {
+    res.status(200).send('Server is healthy');
+  });
 
 // Route Middleware
 app.use('/api/auth', authRoutes);
