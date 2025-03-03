b109f2f3daa34ddd2ef99494530c608788f0b954
Kenechi <kennydukor@gmail.com>
2025-03-03 18:12:56 -0500

    fixed error

--- routes/contributionRoutes.js
+++ routes/contributionRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const { createContribution, getContributionsByCampaign, getContributionsByUser } = require('../controllers/contributionController');
+const { initiatePayment, getContributionsByCampaign, getContributionsByUser } = require('../controllers/contributionController');
 const authMiddleware = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
-router.post('/', authMiddleware, createContribution);
+router.post('/', authMiddleware, initiatePayment);
 router.get('/campaign/:campaignId', getContributionsByCampaign);
 router.get('/user', authMiddleware, getContributionsByUser);
 
