2f659c4566dcfe21b240ae9a761c71e3eb110269
Mmesomachukwu_O <okeoma234@gmail.com>
2025-08-03 10:39:46 -0400

    Added initialize payment route and removed console logs from resend OTP

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { verifyPaystackPayment, verifyPayPalPayment } = require('../controllers/paymentController');
+const {generatePaymentLink} = require("../services/paymentService");
 
 const router = express.Router();
 
 router.get('/paystack/verify', verifyPaystackPayment);
 router.get('/paypal/verify', verifyPayPalPayment);
-
+router.post('/initializePayment', generatePaymentLink);
 module.exports = router;
