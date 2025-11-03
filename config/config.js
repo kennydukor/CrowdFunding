b561d43c01e14a1b11a6113cc7353d8915a810f3
owujib <owujibfavour@gmail.com>
2025-11-03 01:41:00 +0100

    added config

---
+++ config/config.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+
+module.exports = {
+ development: {
+  username: process.env.DB_USER || 'postgres',
+  password: process.env.DB_PASSWORD || 'password',
+  database: process.env.DB_NAME || 'my_app_dev',
+  host: process.env.DB_HOST || '127.0.0.1',
+  port: process.env.DB_PORT || 5432,
+  dialect: 'postgres',
+  logging: console.log,
+ },
+ test: {
+  username: process.env.DB_USER || 'postgres',
+  password: process.env.DB_PASSWORD || 'password',
+  database: process.env.DB_NAME || 'my_app_test',
+  host: process.env.DB_HOST || '127.0.0.1',
+  port: process.env.DB_PORT || 5432,
+  dialect: 'postgres',
+  logging: false,
+ },
+ production: {
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: 'postgres',
+  logging: false,
+  ...(process.env === 'production' && {
+   dialectOptions: {
+    ssl: {
+     require: true,
+     rejectUnauthorized: false,
+    },
+   },
+  }),
+ },
+};
